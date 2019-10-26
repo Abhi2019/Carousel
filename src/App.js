6331db0cf59366a51eb58fc8ecc52c0c5069beb4
Abhijit Patra <rintu.patr22@gmail.com>
2019-10-27 04:54:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carosuel from './container/carousel/Carousel'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Carosuel></Carosuel>
+    </div>
+  );
+}
+
+export default App;
